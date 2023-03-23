b75c14674237eed3494a4aa60f8c10d85c485936
Justin <justin@kaizenmarketing.io>
2023-03-22 23:26:43 -0300

    started the process of replacing base 64 images

--- src/editor/Editor.jsx
+++ src/editor/Editor.jsx
@@ -3,7 +3,6 @@
 import 'grapesjs-preset-webpage';
 import { GrapesjsReact } from 'grapesjs-react';
 import webpageDefaultPlugin from 'grapesjs-preset-webpage';
-import exportToStorage from './plugins/export-to-storage'
 import InnerHtml from 'dangerously-set-html-content'
 import { useEffect, useState } from 'react';
 import CalendlyFormPlugin from './components/CalendlyForm';
@@ -18,29 +17,6 @@ export default function Editor({ content, saveContent }) {
 
     const css = `<link href="https://unpkg.com/grapesjs@0.20.3/dist/css/grapes.min.css" rel="stylesheet"/>`
 
-    const uri = '/api/save-content'
-
-    const pushContent = async (editor) => {
-        const payload = JSON.stringify({
-            id: contentId,
-            grapesjsContent: await editor.getProjectData(),
-            content: {
-                html: editor.getHtml(),
-                css: editor.getCss()
-            },
-        })
-        const options = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: payload
-        };
-
-        fetch(uri, options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));  
-    }
-
     useEffect(()=>{
         if(content && editor){
             editor.loadProjectData(content)
@@ -50,7 +26,30 @@ export default function Editor({ content, saveContent }) {
     },[editor, content])
 
     const handleSaveButtonClick = async () => {
-        saveContent(await editor.getProjectData())
+        const projectData = await editor.getProjectData()
+        const recursivelyReplaceBase64 = (item) => {
+            if(typeof item !== 'object'){return}
+            if(Array.isArray(item)){
+                item.forEach((subItem)=>{
+                    recursivelyReplaceBase64(subItem)
+                })
+                return
+            }
+            const src = item.src
+            if(typeof src !== 'string'){return}
+            if(src.startsWith('data:image/')){
+                //todo add storage bucket upload here
+                //todo change to my rust api to not expose bucket keys
+
+                console.log(src)
+            }
+            Object.keys(item).forEach((key)=>{
+                recursivelyReplaceBase64(item[key])
+            })
+        }
+        recursivelyReplaceBase64(projectData.assets)
+        console.log(projectData)
+        //saveContent(projectData)
     }
     
 
@@ -73,9 +72,6 @@ export default function Editor({ content, saveContent }) {
                     }
                 }
             }),
-            editor => exportToStorage(editor, {
-                pushContent
-            }),
             // editor => grapesjsTailwind(editor, {}),
             editor => CalendlyFormPlugin(editor, {}),
             editor => List(editor, {}),
@@ -86,14 +82,14 @@ export default function Editor({ content, saveContent }) {
         canvas = {{
             styles: [
                 'https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
-                'https://fonts.googleapis.com/css2?family=Inter:wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+                'https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap'
             ]
         }}
         allowScripts={true}
         onInit={async(editor)=>{
             setEditor(editor)
             editor.on('load', ()=>{
-                document.getElementById('grapesjs-react').style = "width: 100%; height: 95vh;"
+                // document.getElementById('grapesjs-react').style = "width: 100%; height: 95vh;"
 
                 let styleManager = editor.StyleManager;
                 let typographySector = styleManager.getSector('typography');
@@ -101,6 +97,7 @@ export default function Editor({ content, saveContent }) {
                 let list = fontProperty.attributes.options;
                 list.push(
                     { value: 'Barlow Semi Condensed', name: 'Barlow Semi Condensed' },
+                    { value: 'Architects Daughter', name: 'Architects Daughter' },
                 );
                 styleManager.render();
 
